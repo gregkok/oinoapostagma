026b88622fd51113e63b1333a080fc7c5110c4b7
gregkok <grkokkoris@gmail.com>
2019-09-12 18:53:24 +0200

    until second image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FirstImageComponent } from './first-image/first-image.component';
+import { SecondImageComponent } from './second-image/second-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FirstImageComponent,
+    SecondImageComponent,
   ],
   imports: [
     BrowserModule
