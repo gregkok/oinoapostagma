0a31d309669d03f8e0276c666b6d465940f9c7e4
gregkok <grkokkoris@gmail.com>
2019-09-28 14:12:33 +0200

    some menu items and different background tryout

--- src/app/menu-page/menu-page.component.ts
+++ src/app/menu-page/menu-page.component.ts
@@ -7,77 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPageComponent implements OnInit {
 
-  salad;
-  meat;
-  type;
-  name;
-  price;
-
-  menuObjectOne = [
-    this.salad = {
-      type: 'Meats',
-      list:{
-        name: 'choriatiki',
-        price: 12
-      }
-    },
-    this.meat = [
-      {
-        type: 'Meats',
-        name: 'choriatiki',
-        price: 12
-      },
-      {
-        type: 'Meats',
-        name: 'saganaki',
-        price: '12'
-      },
-      {
-        type: 'Meats',
-        name: 'ntakos',
-        price: '14'
-      }
-    ]
-  ];
-
-  menuObjectTwo = [
-    this.salad = {
-      tittle: 'Salads',
-      choriatiki: {
-        name: 'choriatiki',
-        price: 12
-      },
-      saganaki: {
-        name: 'saganaki',
-        price: '12'
-      },
-      ntakos: {
-        name: 'ntakos',
-        price: '14'
-      }
-    },
-    this.meat = {
-      tittle: 'Salads',
-      choriatiki: {
-        name: 'choriatiki',
-        price: 12
-      },
-      saganaki: {
-        name: 'saganaki',
-        price: '12'
-      },
-      ntakos: {
-        name: 'ntakos',
-        price: '14'
-      }
-    }
-  ];
-
   constructor() { }
 
   ngOnInit() {
   }
-
-
-
 }
