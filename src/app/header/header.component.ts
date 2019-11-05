201902d5b8c638eb807264539ba9d8199ee17016
gregkok <grkokkoris@gmail.com>
2019-11-05 23:34:27 +0100

    menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,6 +16,11 @@ isMenuClicked = false;
   onMenuClick() {
       $('#nav-container').toggleClass('pushed');
       this.isMenuClicked = !this.isMenuClicked;
+      // if (this.isMenuClicked) {
+      //   document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+      // } else {
+      //   document.getElementsByTagName('body')[0].style.overflow = '';
+      // }
   }
 
 }
