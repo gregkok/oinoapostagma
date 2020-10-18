604c5d6a909ff9c09941dee41875b5f512dec63a
gregkok <grkokkoris@gmail.com>
2020-10-18 20:41:19 +0200

    add remove images

--- src/app/gallery/gallery/gallery-model.ts
+++ src/app/gallery/gallery/gallery-model.ts
@@ -8,62 +8,37 @@ export const gallery: Gallery[] = [
     {
         id: 0,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503803.jpg',
+        src: 'assets/images/galleryImages/850_3821.jpg',
     },
     {
         id: 1,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503827.jpg',
+        src: 'assets/images/galleryImages/850_3847.jpg',
     },
     {
         id: 2,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503875.jpg',
+        src: 'assets/images/galleryImages/850_3856.jpg',
     },
     {
         id: 3,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503879.jpg',
+        src: 'assets/images/galleryImages/850_8059.jpg',
     },
     {
         id: 4,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503943.jpg',
+        src: 'assets/images/galleryImages/850_8076.jpg',
     },
     {
         id: 5,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503950.jpg',
+        src: 'assets/images/galleryImages/850_8136.jpg',
     },
     {
         id: 6,
         description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8503988.jpg',
-    },
-    {
-        id: 7,
-        description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8508073.jpg',
-    },
-    {
-        id: 8,
-        description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8508118.jpg',
-    },
-    {
-        id: 9,
-        description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8508136.jpg',
-    },
-    {
-        id: 10,
-        description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8508175.jpg',
-    },
-    {
-        id: 11,
-        description: 'Σαλάτα',
-        src: 'assets/images/galleryImages/8508264.jpg',
+        src: 'assets/images/galleryImages/850_8264.jpg',
     },
     {
         id: 13,
